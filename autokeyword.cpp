a096f6369a6110a3b1f44ffcb4f7c7461efede00
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-20 15:15:03 +0530

    auto keyword in c++ 11

---
+++ autokeyword.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	auto x=4;
+	auto y=3.14;
+	auto z='a';
+	auto p="Rishabh";
+	cout<<"size of x="<<sizeof(x)<<" value of x="<<x<<endl;
+	cout<<"size of y="<<sizeof(y)<<" value of y="<<y<<endl;
+	cout<<"size of z="<<sizeof(z)<<" value of z="<<z<<endl;
+	cout<<"size of p="<<sizeof(p)<<" value of p="<<p<<endl;
+	return 0;
+}
