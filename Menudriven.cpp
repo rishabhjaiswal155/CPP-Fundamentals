6adafc21cf872374b0ba70ed469753dcb5afaaec
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-20 17:55:30 +0530

    Menu driven program using do-while loop

---
+++ Menudriven.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=20,b=10;
+	int choice;
+	cout<<"----------menu-------------"<<endl;
+	cout<<"1.addition\n 2.subtraction\n 3.mutiplication\n 4.division \n"<<endl;
+	do{
+	cout<<"Please enter the choice"<<endl;
+	cin>>choice;
+	switch(choice){
+		case 1:
+			cout<<"Addition="<<a+b<<endl;
+			break;
+			
+			case 2:
+				cout<<"Subtraction="<<a-b<<endl;
+				break;
+				
+				case 3:
+					cout<<"Mutiplication="<<a*b<<endl;
+					break;
+					
+					case 4:
+						cout<<"Divison="<<a/b<<endl;
+						break;
+						default:
+							cout<<"Invalid choice"<<endl;
+							break;
+	}
+}while(choice<=4);
+	return 0;
+}
