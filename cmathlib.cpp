fdeb1c20942b64882c56d982aa8a941a90ad7341
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-21 17:14:51 +0530

    cmath library and its inbuilt functions

---
+++ cmathlib.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	const int x=10;
+	//x++;
+	int i=56,j=98;
+	cout<<"maximum= "<<max(i,j)<<endl;
+	cout<<"Square root of 9 is "<<sqrt(9)<<endl;
+	cout<<"power of 3 to 3 is "<<pow(3,3)<<endl;
+	return 0;
+}
