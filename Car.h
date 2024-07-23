afc4db4d7339903174b0f6b68adad53e5736c413
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-23 19:28:03 +0530

    Car inheritance example with Virtual functions,Base class pointer,runtime binding

---
+++ Car.h
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+class Car{
+	private:
+		int modelno;
+		string name,color;
+	protected:
+		double price;
+		
+	public:
+		Car(){
+			cout<<"----Default car constructor get invoked------"<<endl;
+			modelno=1001;
+			name="car";
+			color="white";
+			price=300000;
+		}
+		Car(int modelno,string name,string color,double price){
+			cout<<"----paramterize car constructor invoked----"<<endl;
+			this->modelno=modelno;
+			this->name=name;
+			this->color=color;
+			this->price=price;
+		}
+		virtual void speed(){
+			cout<<"------------spped of car is 80KMPH------"<<endl;
+		}
+		virtual void display(){
+			cout<<modelno<<" "<<name<<" "<<color<<" "<<price<<endl;
+		}
+};
+
+class Sedan :public Car{
+	private:
+		int airbags;
+	public:
+		Sedan(int modelno,string name,string color,double price,int airbags):Car(modelno,name,color,price){
+			cout<<"Parameterize sedan constructor get invoked"<<endl;
+			this->airbags=airbags;
+		}
+		void sedanFun(){
+			cout<<"sedan car functionality"<<endl;
+		}
+		void speed(){
+			cout<<"-----speed of Sedan car function is 180 KMPH----"<<endl;
+		}
+		
+		void dipslay(){
+			Car::display();
+			cout<<"Sedan car specific details"<<endl;
+			cout<<"Airbags="<<airbags<<endl;
+		}
+};
+
+class Sportscar:public Car{
+	private:
+		int airbags;
+		int nos;
+	public:
+		Sportscar(int modelno,string name,string color,double price,int airbags,int nos):Car(modelno,name,color,price){
+			cout<<"Parameterize Sportscar constructor invoked"<<endl;
+			this->airbags=airbags;
+			this->nos=nos;
+		}
+		void startingNos(){
+			cout<<"Starting NOS in sportscar"<<endl;
+		}
+		void speed(){
+			cout<<"Speed of sportsCar is 300 KMPH"<<endl;
+		}
+		void display(){
+			Car::display();
+			cout<<"Sportscar specific details"<<endl;
+			cout<<airbags<<" "<<nos<<endl;
+		}
+};
---
+++ TesterCar.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include"Car.h"
+using namespace std;
+int main(){
+	cout<<"------Car Class-----"<<endl;
+	Car c;
+	c.speed();
+	c.display();
+	Car c1(5001,"Alto","white",300000);
+	c1.speed();
+	c1.display();
+	
+	cout<<"--------Sedan class-------"<<endl;
+	Sedan s(6001,"Dzire","blue",800000,4);
+	s.speed();
+	s.sedanFun();
+	s.display();
+	
+	cout<<"----------Sportscar class-------"<<endl;
+	Sportscar spc(8001,"Ferrari","Red",800000,4,2);
+	spc.speed();
+	spc.startingNos();
+	spc.display();
+	
+	Car *ptr=&s;
+	ptr->speed();
+	ptr->display();
+	
+	Car *cars[3];
+	Car car(7001,"Indica","Grey",450000);
+	Sedan sedan(8001,"Ciaz","Coffee",850000,5);
+	Sportscar sport(4001,"Lambhorghini","Yellow",800000,2,2);
+	
+	cars[0]=&car;
+	cars[1]=&sedan;
+	cars[2]=&sport;
+	
+	for(int i=0;i<3;i++){
+		cars[i]->display();
+	}
+	
+	return 0;
+}
